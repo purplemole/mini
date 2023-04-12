2e85851b6efd15dd1623aea573dc86bbed427749
taetae <68893329+TaeKyeong-coder@users.noreply.github.com>
2023-04-12 17:39:02 +0900

    TextFieldState class 추가 (Test 포함)
    ## TextFieldState 클래스
    * AdminPageView 내부 composable 들 값을 DataStore 에 저장
    * string 의 state값 저장 필요
    * 허가 된 composable 에서 전부 접근 가능하게 해야 함.
    * staticCompositionLocal값으로 사용할 수 있게 해 주는 클래스

---
+++ app/src/main/java/com/clobot/mini/util/TextFieldSate.kt
@@ -0,0 +1,17 @@
+package com.clobot.mini.util
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+
+class TextFieldState {
+    private val _text = mutableStateOf("")
+    val text: State<String> = _text
+
+    fun setText(value: String) {
+        _text.value = value
+    }
+
+    fun getText(): String {
+        return text.value
+    }
+}
---
+++ app/src/test/java/com/clobot/mini/TextFieldStateTest.kt
@@ -0,0 +1,22 @@
+package com.clobot.mini
+
+import com.clobot.mini.util.TextFieldState
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TextFieldStateTest {
+    @Test
+    fun testSetText() {
+        val textFieldState = TextFieldState()
+        textFieldState.setText("test1")
+        assertEquals("test1", textFieldState.text.value)
+    }
+
+    @Test
+    fun testGetText(){
+        val textFieldState = TextFieldState()
+        textFieldState.setText("test2")
+        val returnText = textFieldState.getText()
+        assertEquals("test2", textFieldState.text.value, returnText)
+    }
+}
