0eefa734e823897ac67d015dea711897e29d4d33
heechan <ryanhe4@gmail.com>
2023-05-03 15:54:43 +0900

    comment: getCurTimeInfo 주석 변경

--- app/src/main/java/com/clobot/mini/util/getCurTimeInfo.kt
+++ app/src/main/java/com/clobot/mini/util/getCurTimeInfo.kt
@@ -4,10 +4,7 @@ import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
 
 /**
- * @param formatType YMD - 년 월 일,
- * YMDE - 년(20XX) 월 일 요일,
- * HOUR12 - 시(1~12) 분 초,
- * HOUR24 - 시(0~23) 분 초
+ * @param formatType  포맷 타입
  * @param currentTime 시간 정수값
  */
 @SuppressLint("SimpleDateFormat")
@@ -15,11 +12,18 @@ fun getCurTimeInfo(formatType: DateFormat, currentTime: Long): String {
     return formatType.format(currentTime)
 }
 
+
+/**
+ * @sample YMD
+ * @sample YMDE
+ * @sample HOUR12
+ * @sample HOUR24
+ * */
 enum class DateFormat(private val format: String) {
     YMD("yyyy-MM-dd"),
     YMDE("yy-MM-dd-E"),
     HOUR12("hh:mm:ss"),
     HOUR24("HH:mm:ss");
 
-    fun format(time: Long):String = ( SimpleDateFormat(this.format).format(time) )
+    fun format(time: Long): String = (SimpleDateFormat(this.format).format(time))
 }
